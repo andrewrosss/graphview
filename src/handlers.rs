ff613c9c8f8506fdd098ed1f055a82e13211521e
Andrew Ross <andrewrosss@fb.com>
2023-05-30 20:59:50 -0400

    Flesh-out routes

---
+++ src/handlers.rs
@@ -0,0 +1,61 @@
+use serde;
+use axum::extract::Form;
+use axum::extract::Path;
+
+
+#[derive(serde::Deserialize)]
+struct CreateGraphRequest {
+    dot: String,
+}
+
+pub struct Root {}
+
+impl Root {
+    pub async fn get() {
+        println!("root page");
+
+        // Show dot editor
+        todo!()
+    }
+
+    pub async fn post(form: Form<CreateGraphRequest>) {
+        // save dot to the database
+        println!("dot: {}", form.dot);
+
+        // redirect to the graph detail page
+        todo!()
+    }
+}
+
+#[derive(serde::Deserialize)]
+struct UpdateGraphRequest {
+    dot: String,
+}
+
+pub struct GraphDetail {}
+
+impl GraphDetail {
+    pub async fn get(Path(id): Path<String>) {
+        // get dot from the database
+        println!("id: {}", id);
+
+        // render the graph detail page
+        todo!()
+    }
+
+    pub async fn patch(Path(id): Path<String>, form: Form<UpdateGraphRequest>) {
+        // update dot in the database
+        println!("id: {}, dot: {}", id, form.dot);
+
+        // redirect to the graph detail page
+        todo!()
+    }
+
+    pub async fn delete(Path(id): Path<String>) {
+        // delete dot from the database
+        println!("id: {}", id);
+
+        // redirect to the root page
+        todo!()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+mod handlers;
+
+use axum::routing::get;
+use axum::routing::post;
+use axum::Router;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route('/', get(handlers::Root::get).post(handlers::Root::post))
+        .route(
+            "/:id",
+            get(handlers::GraphDetail::get)
+                .patch(handlers::GraphDetail::patch)
+                .delete(handlers::GraphDetail::delete),
+        );
 }
