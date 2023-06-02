0a443afbef979c51d8c9c737c4af55722c48c47e
Andrew Ross <andrew.ross.mail@gmail.com>
2023-06-02 11:38:03 -0400

    Fix tracing to show request logs

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,24 @@ use std::net::SocketAddr;
 use axum::routing::get;
 use axum::Router;
 use tower_http::services::ServeDir;
+use tower_http::trace::DefaultMakeSpan;
+use tower_http::trace::DefaultOnResponse;
 use tower_http::trace::TraceLayer;
+use tracing::Level;
 
 #[tokio::main]
 async fn main() {
     // Setup tracing
-    tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::DEBUG)
-        .init();
+    tracing_subscriber::fmt().init();
+    let tracing_layer = TraceLayer::new_for_http()
+        .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
+        .on_response(DefaultOnResponse::new().level(Level::INFO));
 
+    // serve static files
     let public = ServeDir::new("public");
+
+    // Build the application
     let app = Router::new()
-        .layer(TraceLayer::new_for_http())
         .route("/", get(handlers::Root::get).post(handlers::Root::post))
         .route(
             "/g/:id",
@@ -24,10 +30,10 @@ async fn main() {
                 .patch(handlers::GraphDetail::patch)
                 .delete(handlers::GraphDetail::delete),
         )
-        .fallback_service(public);
+        .fallback_service(public)
+        .layer(tracing_layer);
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    axum::Server::bind(&addr)
+    axum::Server::bind(&SocketAddr::from(([127, 0, 0, 1], 3000)))
         .serve(app.into_make_service())
         .await
         .unwrap();
