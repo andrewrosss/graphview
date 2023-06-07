d8d29804415c66e4993553e2216d5def157c5a5b
Andrew Ross <andrew.ross.mail@gmail.com>
2023-06-07 00:57:33 -0400

    Update src/static/index.js
    Improve url hash <-> textarea synchronization

--- src/static/index.js
+++ src/static/index.js
@@ -1,6 +1,8 @@
 /** @type {HTMLTextAreaElement | null} */
 const textarea = document.getElementById('dot');
+const textareaValue = textarea?.value;
 
+// sync textarea value to URL hash
 textarea?.addEventListener('input', (event) => {
   const dot = event.target.value;
   const compressed = LZString.compressToEncodedURIComponent(dot);
@@ -9,21 +11,33 @@ textarea?.addEventListener('input', (event) => {
   history.replaceState(null, '', url);
 });
 
+// sync URL hash and textarea value on page load if one is set and the other is not
 window.addEventListener("DOMContentLoaded", () => {
   const url = new URL(location);
   const compressed = url.hash.slice(1);
-  if (!compressed) {
+  if (!compressed && !textareaValue) {
     return;
   }
-  const dot = LZString.decompressFromEncodedURIComponent(compressed);
-  textarea.value = dot;
-  textarea.dispatchEvent(new Event('input'));
+  // url hash is set but textarea is not
+  if (compressed && !textareaValue) {
+    const dot = LZString.decompressFromEncodedURIComponent(compressed);
+    textarea.value = dot;
+    return;
+  }
+  // textarea is set but url hash is not
+  if (!compressed && textareaValue) {
+    const dot = textareaValue;
+    const compressed = LZString.compressToEncodedURIComponent(dot);
+    url.hash = compressed;
+    history.replaceState(null, '', url);
+    return;
+  }
 });
 
+// copy url to clipboard
 /** @type {HTMLButtonElement | null} */
 const button = document.getElementById('copy-url-button');
 const buttonText = button?.textContent;
-
 
 let copyUrlButtonTimeout = null;
 button?.addEventListener('click', () => {
@@ -36,6 +50,7 @@ button?.addEventListener('click', () => {
     button.textContent = previousText;
   }, 2000);
 
+  // update the url hash and copy to clipboard
   const dot = textarea.value;
   const compressed = LZString.compressToEncodedURIComponent(dot);
   const url = new URL(location);
